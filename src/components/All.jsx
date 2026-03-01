250237634d13b5492ef4693fc4ce3ad57e56ff27
Yusuf-AI2011 <yusufshomurodov36@gmail.com>
2026-03-01 15:19:29 +0500

    changed

--- src/components/All.jsx
+++ src/components/All.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const All = () => {
-  return <div>
-    <h1>All</h1>
-  </div>;
+  const [product, steProduct] = useState([]);
+  const api = `https://dummyjson.com/products`;
+
+  useEffect(() => {
+    axios.get(api).then((data) => {
+      console.log(data?.data?.products);
+      steProduct(data?.data?.products);
+    });
+  }, []);
+
+  return (
+    <div className="All">
+      <h1 className="all__title">All Products</h1>
+      <div className="all__cards">
+        {product.map((item) => (
+          <div key={item.id} className="all__card">
+            <div className="all__image-wrapper">
+              <img src={item.images[0]} alt="" className="all__image" />
+            </div>
+
+            <p className="all__category">Category: {item.category}</p>
+            <p className="all__brand">Brand: {item.brand}</p>
+            <button className="all__button">Throw to basket</button>
+          </div>
+        ))}
+      </div>
+      <button className="all__bigButton">View All</button>
+    </div>
+  );
 };
 
 export default All;
